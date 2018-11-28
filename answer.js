e9b63bf88b7877d4ee60588a6d550299c61c1881
Christopher Pace <Christopher_pace@ymail.com>
2018-11-28 14:04:34 -0600

    check in

---
+++ answer.js
@@ -0,0 +1,13 @@
+array = [[2, 3], [4, 3, 2]];
+for(i=0;i<array.length;i++)
+    var array2= array+""
+//  if(i<=0||i>=0){
+   //   var matharray
+   //  }
+   for(x=0;x<array2.length;x++)
+       var array3 = array2+""
+
+    console.log(array2);
+
+
+
